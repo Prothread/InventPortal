8027a096a620ff6842ced03de34379151c748797
Gijsvdabeele <gijsvdabeele@gmail.com>
2016-10-04 14:54:37 +0200

    Check voor upload

--- app/Model/upload.php
+++ app/Model/upload.php
@@ -6,17 +6,38 @@
  * Time: 11:47
  */
 
+$error = 0;
 if (isset($_FILES['myFile'])) {
     $myFile = $_FILES['myFile'];
     $fileCount = count($myFile["name"]);
     for ($i = 0; $i < $fileCount; $i++) {
 
-        $test = $myFile['name'][$i];
-        $test1 = $myFile['tmp_name'][$i];
+            $test = $myFile['name'][$i];
+            $test1 = $myFile['tmp_name'][$i];
 
             $target_dir = "../app/uploads/";
             $target_file = $target_dir . $test;
-        if (!file_exists($target_file)) {
+            $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+echo $test;
+        if(file_exists($target_file)){
+            $error = 1;
+            echo $test." already exists!";
+            ?><br /><?php
+        }
+
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+            $error = 1;
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            ?><br /><?php
+        }
+
+        if(filesize($test) > 50000000){
+            $error = 1;
+            echo $test." File too big!";
+            ?><br /><?php
+        }
+
+        if ($error == 0) {
             $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
             if (move_uploaded_file($test1, $target_file)) {
 
@@ -35,8 +56,7 @@ if (isset($_FILES['myFile'])) {
                 <?php
             }
         }else{
-            echo "The file " . $test . " already exists!.";
-            ?><br /><?php
+
         }
     }
 }
