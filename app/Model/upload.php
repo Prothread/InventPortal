bc6f3271f44132a63432fa477f2fac2825b6c2c0
Gijsvdabeele <gijsvdabeele@gmail.com>
2016-10-04 11:48:47 +0200

    Multiple upload function

--- app/Model/upload.php
+++ app/Model/upload.php
@@ -5,17 +5,19 @@
  * Date: 29-Sep-16
  * Time: 12:47
  */
-$fileCount = count($_FILES['fileToUpload']["name"]);
+
+
+/*
+$fileCount = count($_FILES['myFile']["name"]);
 for ($i = 0; $i < $fileCount; $i++) {
 
-    $target_dir = "../app/uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
     $uploadOk = 1;
-    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+
 
 // Check if image file is a actual image or fake image
     if (isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        $check = getimagesize($_FILES["myFile"]["tmp_name"]);
         if ($check !== false) {
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
@@ -47,10 +49,47 @@ for ($i = 0; $i < $fileCount; $i++) {
             // if everything is ok, try to upload file
         } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
-            echo '<img src="../app/uploads/'.basename($_FILES["fileToUpload"]["name"]).'">';
+           // echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+           // echo '<img src="../app/uploads/'.basename($_FILES["fileToUpload"]["name"]).'">';
+
+
+
+
         } else {
             echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}
+*/
+
+if (isset($_FILES['myFile'])) {
+    $myFile = $_FILES['myFile'];
+    $fileCount = count($myFile["name"]);
+    for ($i = 0; $i < $fileCount; $i++) {
+
+        $test = $myFile['name'][$i];
+        $test1 = $myFile['tmp_name'][$i];
+
+            $target_dir = "../app/uploads/";
+            $target_file = $target_dir . $test;
+        if (!file_exists($target_file)) {
+            $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+            if (move_uploaded_file($test1, $target_file)) {
+
+                echo "The file " . $test . " has been uploaded.";
+                echo '<img src="../app/uploads/' . $test . '">';
+                ?>
+
+                <p>File #<?= $i + 1 ?>:</p>
+                <p>
+                    Name: <?= $myFile["name"][$i] ?><br>
+                    Temporary file: <?= $myFile["tmp_name"][$i] ?><br>
+                    Type: <?= $myFile["type"][$i] ?><br>
+                    Size: <?= $myFile["size"][$i] ?><br>
+                    Error: <?= $myFile["error"][$i] ?><br>
+                </p>
+                <?php
+            }
         }
     }
 }
--- app/view/uploadoverzicht.php
+++ app/view/uploadoverzicht.php
@@ -6,8 +6,22 @@
  * Time: 12:47
  */
 ?>
+<!--
 <form action="?page=uploading" method="post" enctype="multipart/form-data">
     Select an image to upload
     <input type="file" name="fileToUpload" id="fileToUpload" multiple="multiple">
     <input type="submit" value="Upload image" name="submit">
 </form>
+-->
+<!doctype html>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+<form action="?page=uploading" method="post" enctype="multipart/form-data">
+    <input type="file" name="myFile[]" id="myFile" multiple>
+    <input type="submit" value="Upload image" name="submit">
+</form>
+</body>
+</html>
