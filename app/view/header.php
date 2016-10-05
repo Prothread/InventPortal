5fe6ebd57216d9d37b940cc287623edf00d266f8
Prothread <kevin.herdershof@hotmail.com>
2016-10-05 15:14:46 +0200

    rip
    vergat er 1

--- app/view/header.php
+++ app/view/header.php
@@ -6,10 +6,7 @@
  */
 ?>
 <html lang="nl">
-
 <head>
-
-    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
     <meta name="description" content="Madalco Klantenportaal">
@@ -47,124 +44,52 @@
     <![endif]-->
 
 
-    <script>
-        function searchTable() {
-            // Declare variables
-            var input, filter, table, tbody, tr, td, i;
-            input = document.getElementById("TableInput");
-            filter = input.value.toUpperCase();
-            table = document.getElementById("overzicht");
-            tbody = table.getElementsByTagName("tbody");
-            tr = table.getElementsByTagName("tr");
-            td = table.getElementsByTagName("td");
-
-            // Loop through all table rows, and hide those who don't match the search query
-            for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
-                if (td) {
-                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = "";
-                    } else {
-                        tr[i].style.display = "none";
-                        tr[0].style.display = "";
-                    }
-                }
-            }
-        }
-    </script>    <script type="text/javascript">
-        $(document).ready(function(){
-
-            // Text van input halen
-            $(".suggestionsinput").click(function(){
-                if($(".suggestionsinput").val() == "Search")
-                {
-                    $(".suggestionsinput").val("");
-                }
-            })
-
-            // Checken of de button ingedrukt is
-            $(".suggestionsinput").keyup(function(event){
-                if($(".suggestionsinput").val() != "")
-                {
-                    // make suggestions visible
-                    $("#suggestions").css('visibility', 'visible');
-                    $("#suggestions").hide();
-                    $("#suggestions").fadeIn('slow');
-                    $(".searchterm").text($(".suggestionsinput").val());
-                    // $("#suggestions").load('http://URL.to.load');
-
-                } else {
-                    // Suggesties verbergen
-                    $("#suggestions").fadeOut('slow', function(){
-                        $("#suggestions").css('visibility', 'hidden');
-                    });
-
-                }
-            })
-        })
-    </script>
 
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <!-- Script voor preview uploaden -->
-    <script>
-        var loadFile = function(event) {
-            oldimg = $('.preview').attr('src');
-            var preview = document.getElementById('preview');
-            preview.src = URL.createObjectURL(event.target.files[0]);
-            newimg = preview.src;
-            if(newimg.indexOf('/null') > -1) {
-                preview.src = oldimg;
-            }
-        };
-
-        $('.submit-button').on('click', function(event) {
-            alert('This is a dummy submit button. It does nothing.');
-            event.preventDefault();
-        });
-    </script>
 </head>
 
 <body>
-<div id="header">
-    <div id="MenuSide">
-        <div id="MenuButton">
-            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><img src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-20.png"></a>
+    <div id="header">
+        <div id="MenuSide">
+            <div id="MenuButton">
+                <a href="#menu-toggle" class="btn btn-default" id="menu-toggle"><img src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-20.png"></a>
+            </div>
         </div>
-    </div>
 
-    <div id="NameSide">
-        <div id="UserPhoto">
+        <div id="NameSide">
+            <div id="UserPhoto">
+            </div>
+            <h3 id="LoggedInAs">Testadmin</h3>
+            <a id="LogOut" href="#"><h6 id="LogOut">Uitloggen</h6></a>
         </div>
-        <h3 id="LoggedInAs">Testadmin</h3>
-        <a id="LogOut" href="#"><h6 id="LogOut">Uitloggen</h6></a>
     </div>
-</div>
 
     <div id="wrapper">
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
-
                 <li class="nav-button-home">
                     <a href="index.php?page=dashboard">Home</a>
                 </li>
+
                 <li class="nav-button-upload">
                     <a href="index.php?page=phpmail">Upload</a>
                 </li>
+
                 <li class="nav-button-all">
                     <a href="index.php?page=overzicht">Overzicht</a>
                 </li>
+
                 <li class="nav-button-settings">
                     <a href="index.php?page=#">Instellingen</a>
                 </li>
-                <br>
+
                 <li class="nav-button-logout">
                     <a href="#">Uitloggen</a>
                 </li>
             </ul>
         </div>
-        <!-- /#sidebar-wrapper -->
