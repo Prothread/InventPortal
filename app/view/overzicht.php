3931464096303d9fa70d14214b844b0e19ab93ae
jvalckx <valckxj@outlook.com>
2016-10-11 10:10:37 +0200

    Veranderingen
    Veranderingen

--- app/view/overzicht.php
+++ app/view/overzicht.php
@@ -51,7 +51,15 @@ $uploads = new BlockController();
                                 <?= date("d-m-Y", strtotime($upload['datum']));?>
                             </td>
                             <td>
-                                <?= $upload['verified']?>
+                              <?php if ($upload['verified'] == 1) {?>
+                                    <img alt="Gezien" style="width: 50px; height: 50px;" src="../public/icons/gezien.png">
+                                <?php } elseif ($upload['verified'] == 2) {?>
+                                    <img alt="Geaccepteerd" src="../public/icons/akkoord.png">
+                                <?php } elseif ($upload['verified'] == 3) {?>
+                                    <img alt="Geweigerd" src="../public/icons/geweigerd.png">
+                                <?php } else {?>
+                                    <img alt="Uploaded" src="../public/icons/uploaded.png">
+                                <?php } ?>
                             </td>
                         </tr>
                     <?php }?>
