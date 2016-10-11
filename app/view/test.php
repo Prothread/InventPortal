e9673ee32666dfffa07d2a4b10f35c857104881c
Prothread <kevin.herdershof@hotmail.com>
2016-10-11 09:52:52 +0200

    image draw test

--- app/view/test.php
+++ app/view/test.php
@@ -8,27 +8,71 @@
 
 ?>
 
-<img id="poke" src="../app/uploads/Goodra_9.jpg" alt="Pokemon Goodea" width="220" height="277" />
-<canvas id="canvas0" width="250px" height="300px" style="border:1px solid #d3d3d3;"></canvas>
-<canvas id="canvas1"></canvas>
-<canvas id="canvas2"></canvas>
-<canvas id="canvas3"></canvas>
-<canvas id="canvas4"></canvas>
+<canvas id="canvas0" width="290px" height="400px" style="border:1px solid #d3d3d3;"></canvas>
 
 <script>
+
     var canvas = document.getElementById('canvas0');
-    var context = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
 
-    var canvas1=document.getElementById('canvas1');
-    var canvas2=document.getElementById('canvas2');
-    var canvas3=document.getElementById('canvas3');
-    var canvas4=document.getElementById('canvas4');
+    img = new Image;
+    img.onload = draw;
+    img.src = "../app/uploads/Goodra_9.jpg";
 
-    var img = document.getElementById('poke');
+    img1 = new Image;
+    img1.onload = draw;
+    img1.src = "../public/css/watermerk.png";
 
-    context.drawImage(img,0,0);
-    context.drawImage(canvas1,0,0);
-    context.drawImage(canvas2,100,0);
-    context.drawImage(canvas3,0,100);
-    context.drawImage(canvas4,100,100);
+    function draw() {
+        drawImageProp(ctx, this, 0, 0, canvas.width, canvas.height);
+    }
+
+    function drawImageProp(ctx, img, x, y, w, h, offsetX, offsetY) {
+
+        if (arguments.length === 2) {
+            x = y = 0;
+            w = ctx.canvas.width;
+            h = ctx.canvas.height;
+        }
+
+        /// default offset is center
+        offsetX = typeof offsetX === 'number' ? offsetX : 0.5;
+        offsetY = typeof offsetY === 'number' ? offsetY : 0.5;
+
+        /// keep bounds [0.0, 1.0]
+        if (offsetX < 0) offsetX = 0;
+        if (offsetY < 0) offsetY = 0;
+        if (offsetX > 1) offsetX = 1;
+        if (offsetY > 1) offsetY = 1;
+
+        var iw = img.width,
+            ih = img.height,
+            r = Math.min(w / iw, h / ih),
+            nw = iw * r,   /// new prop. width
+            nh = ih * r,   /// new prop. height
+            cx, cy, cw, ch, ar = 1;
+
+        /// decide which gap to fill
+        if (nw < w) ar = w / nw;
+        if (nh < h) ar = h / nh;
+        nw *= ar;
+        nh *= ar;
+
+        /// calc source rectangle
+        cw = iw / (nw / w);
+        ch = ih / (nh / h);
+
+        cx = (iw - cw) * offsetX;
+        cy = (ih - ch) * offsetY;
+
+        /// make sure source rectangle is valid
+        if (cx < 0) cx = 0;
+        if (cy < 0) cy = 0;
+        if (cw > iw) cw = iw;
+        if (ch > ih) ch = ih;
+
+        /// fill image in dest. rectangle
+        ctx.drawImage(img, cx, cy, cw, ch,  x, y, w, h);
+    }
+
 </script>
