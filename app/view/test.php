d083af1a4f1a3226b0918e1f5bc96b7f6429c987
Prothread <kevin.herdershof@hotmail.com>
2016-10-26 10:06:29 +0200

    Ignore all please

--- app/view/test.php
+++ app/view/test.php
@@ -6,7 +6,117 @@
  * Time: 08:32
  */
 
+
+
 ?>
+
+
+
+<!--<div style="border:0;">
+    <img src="<?/*=  DIR_IMAGE . '4k_22.jpg' */?>" width=309 height=159 />
+    <div style="position:relative; left: 0px; top: -159px; width:150px;">
+        <img src="css/watermerk.png" width=300 height=150>
+    </div>
+</div>-->
+
+<!-- thumbnail image wrapped in a link -->
+
+<a href="#img1">
+    <img src="<?= DIR_IMAGE . '4k_22.jpg'?>" class="thumbnail">
+
+    <div id="thumbnail"></div>
+    <div id="thumbnail2"></div>
+</a>
+<style>
+    .thumbnail {
+        max-width: 40%;
+    }
+    #thumbnail {
+        background: url('css/proef.png') repeat center, url(<?= DIR_IMAGE . '4k_22.jpg'?>);
+        background-size: 100%;
+        max-width: 60%;
+        height: 220px;
+    }
+    #thumbnail2 {
+        background: url('css/proef.png') repeat, url(<?= DIR_IMAGE . '4k_22.jpg'?>) ;
+        background-size: 100px, 100%;
+        max-width: 60%;
+        height: 220px;
+    }
+
+    .italic { font-style: italic; }
+    .small { font-size: 0.8em; }
+
+    /** LIGHTBOX MARKUP **/
+
+    .lightbox {
+        /** Default lightbox to hidden */
+        display: none;
+
+        /** Position and style */
+        position: fixed;
+        z-index: 1001;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        top: 0;
+        left: 0;
+        background: rgba(0,0,0,0.8);
+    }
+
+    .lightbox div {
+        /** Pad the lightbox image */
+        max-width: 90%;
+        max-height: 90%;
+        margin: 0 auto;
+        padding-top: 2%;
+    }
+
+    .lightbox:target {
+        /** Remove default browser outline */
+        outline: none;
+
+        /** Unhide lightbox **/
+        display: block;
+    }
+
+    .lightbox #thumbnail {
+        background: url('css/proef.png') repeat center, url(<?= DIR_IMAGE . '4k_22.jpg'?>);
+        background-size: 100%;
+        max-width: 100%;
+        height:100%;
+    }
+
+    .lightbox #thumbnail2 {
+        background: url('css/proef.png') repeat, url(<?= DIR_IMAGE . '4k_22.jpg'?>) no-repeat;
+        background-size: 13%, 100%;
+        max-width: 100%;
+        height:100%;
+    }
+
+    #lightboximage2 {
+        z-index: 75;
+        position: absolute;
+        padding-top: 18%;
+        padding-left: 44%;
+    }
+    #lightboximage1 {
+        width:100%;
+    }
+</style>
+
+<!--lightbox container hidden with CSS-->
+<a href="#_" class="lightbox" id="img1">
+    <div id="lighter">
+        <!--<img id="lightboximage2" src="css/proef.png">
+        <img id="lightboximage1" src="<?/*= DIR_IMAGE . '4k_22.jpg'*/?>">-->
+
+        <div id="thumbnail2"></div>
+
+    </div>
+</a>
+
+<!--
 <canvas id="canvas" width="290px" height="400px" style="border:1px solid #d3d3d3;"></canvas>
 <script>
 
@@ -86,3 +196,4 @@
     img2.src = dataURL;
 
 </script>
+-->
