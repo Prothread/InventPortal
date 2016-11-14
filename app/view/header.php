80c55b31c97c2a047c460d7ef28783e04c2807e2
jvalckx <valckxj@outlook.com>
2016-11-14 10:13:01 +0100

    DEZE SHIT IS GAAAAAAAAAAAAANDE

--- app/view/header.php
+++ app/view/header.php
@@ -41,15 +41,11 @@ $admin = $settings->getAdminSettings();
     <link rel="stylesheet" href="css/jquery-ui.css">
 
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css">
     <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-
     <script src="http://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-        google.load("jquery", "1.3.2");
-    </script>
+
     <!--Sort table-->
     <script src="js/sorttable.js" type="text/javascript"></script>
 
