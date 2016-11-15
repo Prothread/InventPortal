057903c3bad3bc410c9d2405e3738cb2e29a9ade
Prothread <kevin.herdershof@hotmail.com>
2016-11-15 16:15:33 +0100

    some changessss

--- app/view/test.php
+++ app/view/test.php
@@ -8,8 +8,14 @@ if($user->getPermission($permgroup, 'CAN_SHOW_OVERZICHT') == 1){
 }
 
 $uploads = new BlockController();
+$items = new MailController();
 
-$get_filled_info = $uploads->getUploads();
+$userid = $_SESSION['usr_id'];
+$getAllUserItems = $items->getUserMailByUserId($userid, 0, 0);
+foreach($getAllUserItems as $UserItem) {
+    $mail = $items->getMailById($UserItem['mailid']);
+    $getMails[] = $mail;
+}
 
 ?>
 
@@ -26,48 +32,58 @@ $get_filled_info = $uploads->getUploads();
                 <table id="myTable" class="table table-striped" >
                     <thead>
                     <tr>
+                        <?php if($getMails !== null) {?>
                         <th style="display:none">ID</th>
                         <th>Onderwerp</th>
                         <th>Verstuurder</th>
                         <th>Naam</th>
                         <th>Datum</th>
                         <th>Status</th>
+                        <?php }
+                        else { ?>
+                            <th></th>
+                        <?php } ?>
                     </tr>
                     </thead>
                     <tbody>
                         <?php
-                        foreach ($get_filled_info as $upload) { ?>
-                            <tr>
-                                <td style="display:none">
-                                    <?= $upload['id']; ?>
-                                </td>
-                                <td>
-                                    <a href="?page=item&id=<?= $upload['id'] ?>"><?= $upload['onderwerp'] ?></a>
-                                </td>
-                                <td>
-                                    <?= $upload['verstuurder'] ?>
-                                </td>
-                                <td>
-                                    <?= $upload['naam'] ?>
-                                </td>
-                                <td>
-                                    <?= date("d-m-Y", strtotime($upload['datum'])); ?>
-                                </td>
-                                <td>
-                                    <span style="display:none" id="status"><?= $upload['verified']; ?></span>
-                                    <?php if ($upload['verified'] == 1) { ?>
-                                        <img alt="Gezien" style="width: 50px; height: 50px;"
-                                             src="public/icons/gezien.png">
-                                    <?php } elseif ($upload['verified'] == 2) { ?>
-                                        <img alt="Geaccepteerd" src="public/icons/akkoord.png">
-                                    <?php } elseif ($upload['verified'] == 3) { ?>
-                                        <img alt="Geweigerd" src="public/icons/geweigerd.png">
-                                    <?php } else { ?>
-                                        <img alt="Uploaded" src="public/icons/uploaded.png">
-                                    <?php } ?>
-                                </td>
-                            </tr>
-                        <?php } ?>
+                        if($getMails !== null) {
+                            foreach ($getMails as $upload) { ?>
+                                <tr>
+                                    <td style="display:none">
+                                        <?= $upload['id']; ?>
+                                    </td>
+                                    <td>
+                                        <a href="?page=item&id=<?= $upload['id'] ?>"><?= $upload['onderwerp'] ?></a>
+                                    </td>
+                                    <td>
+                                        <?= $upload['verstuurder'] ?>
+                                    </td>
+                                    <td>
+                                        <?= $upload['naam'] ?>
+                                    </td>
+                                    <td>
+                                        <?= date("d-m-Y", strtotime($upload['datum'])); ?>
+                                    </td>
+                                    <td>
+                                        <span style="display:none" id="status"><?= $upload['verified']; ?></span>
+                                        <?php if ($upload['verified'] == 1) { ?>
+                                            <img alt="Gezien" style="width: 50px; height: 50px;"
+                                                 src="public/icons/gezien.png">
+                                        <?php } elseif ($upload['verified'] == 2) { ?>
+                                            <img alt="Geaccepteerd" src="public/icons/akkoord.png">
+                                        <?php } elseif ($upload['verified'] == 3) { ?>
+                                            <img alt="Geweigerd" src="public/icons/geweigerd.png">
+                                        <?php } else { ?>
+                                            <img alt="Uploaded" src="public/icons/uploaded.png">
+                                        <?php } ?>
+                                    </td>
+                                </tr>
+                            <?php }
+                        }
+                        else { ?>
+                            <td>U heeft nog geen proeven geaccordeerd</td>
+                        <?php }?>
                     </tbody>
                 </table>
             </div>
