a5bf1a4aae0105915caadd2b03c8603529564db2
Paul <pberkhof@student.scalda.nl>
2017-03-23 13:48:21 +0100

    queue pagina gemaakt

---
+++ app/view/queue.php
@@ -0,0 +1,298 @@
+<div id="crm-dashboard-holder">
+
+    <div class="crm-dashboard-row">
+        <header>Open offertes</header>
+
+        <select class="crm-dashboard-select">
+            <option value="" disabled selected>Filter optie</option>
+            <option>Onderwerp</option>
+            <option>Datum</option>
+            <option>Klant</option>
+            <option>Urgentie</option>
+        </select>
+
+        <div class="crm-dashboard-inside-row">
+
+            <button class="custom-file-upload">Aanmaken</button>
+
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline4.png">
+                <ul>
+                    <li>
+                        Offerte onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Offerte onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="crm-dashboard-row">
+        <header>Open projecten</header>
+
+        <select class="crm-dashboard-select">
+            <option value="" disabled selected>Filter optie</option>
+            <option>Onderwerp</option>
+            <option>Datum</option>
+            <option>Klant</option>
+            <option>Urgentie</option>
+        </select>
+
+        <div class="crm-dashboard-inside-row">
+
+            <button class="custom-file-upload">Aanmaken</button>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Project onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="crm-dashboard-row">
+        <header>Open opdrachten</header>
+
+        <select class="crm-dashboard-select">
+            <option value="" disabled selected>Filter optie</option>
+            <option>Onderwerp</option>
+            <option>Datum</option>
+            <option>Klant</option>
+            <option>Urgentie</option>
+        </select>
+
+        <div class="crm-dashboard-inside-row">
+
+            <button class="custom-file-upload">Aanmaken</button>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline3.png">
+                <ul>
+                    <li>
+                        Opdracht onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline3.png">
+                <ul>
+                    <li>
+                        Opdracht onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Opdracht onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="crm-dashboard-row">
+        <header>Open taken</header>
+
+        <select class="crm-dashboard-select">
+            <option value="" disabled selected>Filter optie</option>
+            <option>Onderwerp</option>
+            <option>Datum</option>
+            <option>Klant</option>
+            <option>Urgentie</option>
+        </select>
+
+        <div class="crm-dashboard-inside-row">
+
+            <button class="custom-file-upload">Aanmaken</button>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline4.png">
+                <img class="urgentie" src="css/urgentie4.png">
+                <ul>
+                    <li>
+                        Taak onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline3.png">
+                <img class="urgentie" src="css/urgentie4.png">
+                <ul>
+                    <li>
+                        Taak onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Taak onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline2.png">
+                <ul>
+                    <li>
+                        Taak onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Taak onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="crm-dashboard-row">
+        <header>Open cases</header>
+
+        <select class="crm-dashboard-select">
+            <option value="" disabled selected>Filter optie</option>
+            <option>Onderwerp</option>
+            <option>Datum</option>
+            <option>Klant</option>
+            <option>Urgentie</option>
+        </select>
+
+        <div class="crm-dashboard-inside-row">
+
+            <button class="custom-file-upload">Aanmaken</button>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline3.png">
+                <ul>
+                    <li>
+                        Case onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+
+            <div class="crm-dashboard-box">
+                <img class="deadline" src="css/deadline1.png">
+                <ul>
+                    <li>
+                        Case onderwerp
+                    </li>
+                    <li>
+                        Klant naam
+                    </li>
+                    <li>
+                        04-03-2017
+                    </li>
+                </ul>
+                <a class="toewijzenlink" href="">Toewijzen</a>
+            </div>
+        </div>
+    </div>
+</div>
